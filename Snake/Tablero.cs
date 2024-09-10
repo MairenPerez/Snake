cc3284dbb9c5eb82cf1b0d9c1d2757469519bfe8
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-10 09:48:24 +0200

    Clase Tablero y creación del tablero con el método DibujarTablero()

---
+++ Snake/Tablero.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Tablero
+    {
+        public readonly int Altura;
+        public readonly int Anchura;
+
+        // Constructor
+        public Tablero(int altura, int anchura)
+        {
+            Altura = altura;
+            Anchura = anchura;
+        }
+
+        /// <summary>
+        /// Dibujamos tablero 
+        /// 
+        /// Con un for vamos a dibujar 
+        /// las líneas de arriba y de abajo.
+        /// </summary>
+        public void DibujarTablero()
+        {
+            for (int i = 0; i <= Altura; i++)
+            {
+                // Línea de abajo
+                Console.SetCursorPosition(Anchura, i);
+                Console.WriteLine("<");
+
+                // Línea de arriba
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine(">");
+            }
+        }
+    }
+}
